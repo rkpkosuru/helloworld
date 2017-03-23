3c7acc125d5c7dce14fd991a356dd0c737261a46
rkpkosuru <rkpkosuru@gmail.com>
2017-03-23 18:42:46 -0400

    adding new sop RK

--- HelloWorld/src/main/java/net/ram/spring/controller/HelloController.java
+++ HelloWorld/src/main/java/net/ram/spring/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String hello(Model model) {
 
-		model.addAttribute("name", "John Doe");
+		model.addAttribute("name", "RK");
 
 		return "welcome";
 	}
